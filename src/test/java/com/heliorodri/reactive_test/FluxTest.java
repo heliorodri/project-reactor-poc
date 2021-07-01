527453c974283f56356691ef8617404760405905
heliorodri <heliorodri@github.com>
2021-07-01 13:38:06 -0300

    get countries from list

--- src/test/java/com/heliorodri/reactive_test/FluxTest.java
+++ src/test/java/com/heliorodri/reactive_test/FluxTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FluxTest {
 
     private static final String ARGENTINA = "Argentina";
     private static final String BRAZIL = "Brazil";
     private static final String CANADA = "Canada";
+
+    private static final List<String> COUNTRIES = Arrays.asList(ARGENTINA, BRAZIL, CANADA);
 
     @Test
     public void fluxSubscriber(){
@@ -22,6 +27,13 @@ public class FluxTest {
         Flux<Integer> fluxStarsRate = Flux.range(1,5);
 
         StepVerifier.create(fluxStarsRate).expectNext(1,2,3,4,5).verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberFromList(){
+        Flux<String> countries = Flux.fromIterable(COUNTRIES);
+
+        StepVerifier.create(countries).expectNext(ARGENTINA, BRAZIL, CANADA).verifyComplete();
     }
 
 }
