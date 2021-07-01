cd00efb3559ef5d87329aa575bc2b2b02db1782e
heliorodri <heliorodri@github.com>
2021-07-01 13:31:48 -0300

    flux subscriber

--- src/test/java/com/heliorodri/reactive_test/FluxTest.java
+++ src/test/java/com/heliorodri/reactive_test/FluxTest.java
@@ -1,4 +1,20 @@
 package com.heliorodri.reactive_test;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 public class FluxTest {
+
+    private static final String ARGENTINA = "Argentina";
+    private static final String BRAZIL = "Brazil";
+    private static final String CANADA = "Canada";
+
+    @Test
+    public void fluxSubscriber(){
+        Flux<String> fluxCountries = Flux.just(ARGENTINA, BRAZIL, CANADA).log();
+
+        StepVerifier.create(fluxCountries).expectNext(ARGENTINA, BRAZIL, CANADA).verifyComplete();
+    }
+
 }
